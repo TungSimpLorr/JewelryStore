ff83e138057f6c91641dfc966d99d7b0635e2cc5
Thuhien2005 <ttthuhien2005@gmail.com>
2025-07-07 18:39:19 +0700

    Update blogs-detail.php

--- pages/blogs-detail.php
+++ pages/blogs-detail.php
@@ -27,18 +27,18 @@ if (!$result->num_rows) {
 $bv = $result->fetch_assoc();
 ?>
 
-<link rel="stylesheet" href="/JewelryStore/css/style.css">
-<link rel="stylesheet" href="/JewelryStore/css/blog_detail.css">
+<link rel="stylesheet" href="/Jewelry Store/css/style.css">
+<link rel="stylesheet" href="/Jewelry Store/css/blog_detail.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="/Jewelry%20Store/js/main.js"></script>
-<script src="/Jewelry%20Store/js/process.js"></script>
+<script src="/Jewelry Store/js/main.js"></script>
+<script src="/Jewelry Store/js/process.js"></script>
 <?php include '../includes/header.php'; ?>
 <div class="blog-detail">
     <h1 class="blog-title"><?= htmlspecialchars($bv['tieu_de']) ?></h1>
     
     <div class="blog-meta">
-        <i class="fas fa-user"></i> Người đăng: <?= htmlspecialchars($bv['ten_quan_tri_vien'] ?? 'Không rõ') ?> |
+        <i class="fas fa-user"></i> Người đăng: <?= htmlspecialchars($bv['ho_ten'] ?? 'Không rõ') ?> |
         <i class="fas fa-calendar-alt"></i> Ngày đăng: <?= date("d/m/Y", strtotime($bv['created_at'])) ?>
     </div>
 
@@ -49,8 +49,13 @@ $bv = $result->fetch_assoc();
 
         if (!empty($bv['hinh_anh'])) {
             echo '<div class="blog-middle-image-wrapper">';
-            echo '<img class="blog-middle-image" src="/JewelryStore/images/blogs-detail/' . htmlspecialchars($bv['hinh_anh']) . '" alt="Ảnh giữa bài">';
-            echo '<img class="blog-middle-image" src="/JewelryStore/images/blog-detail/' . htmlspecialchars($bv['hinh_anh']) . '" alt="Ảnh giữa bài">';
+            $img = htmlspecialchars($bv['hinh_anh']);
+            if (preg_match('/^https?:\/\//', $img)) {
+                echo '<img class="blog-middle-image" src="' . $img . '" alt="Ảnh giữa bài">';
+            } else {
+                echo '<img class="blog-middle-image" src="/JewelryStore/images/blogs-detail/' . $img . '" alt="Ảnh giữa bài">';
+                echo '<img class="blog-middle-image" src="/JewelryStore/images/blog-detail/' . $img . '" alt="Ảnh giữa bài">';
+            }
             echo '</div>';
         }
 
